7014fcfbdf553fc3586b1660565e0e7cc78ec799
LauraIniestaRod <laurainiestarod@gmail.com>
2025-03-26 15:54:44 +0100

    correccion estilo flask

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from script import add, subtract
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    a, b = 10, 5  
+    a,b = 10,5 
     # Valores de ejemplo
     sum_result = add(a, b)
     sub_result = subtract(a, b)
@@ -16,6 +17,7 @@ def home():
         "subtraction": f"{a} - {b} = {sub_result}"
     }
     return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
